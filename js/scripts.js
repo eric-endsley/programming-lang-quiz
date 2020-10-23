0c7150937b78cf5eb2cb51f7d129ff1c2a803faf
Eric Endsley <eric.endsley4@gmail.com>
2020-10-23 11:28:42 -0700

    place input values in an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,15 @@
 $(document).ready(function () {
   $("form#code").submit(function(event) {
     event.preventDefault();
-    const cells = $("input:radio[name=cells]:checked").value();
-    const fun = $("input:radio[name=fun]:checked").value();
-    const fear = $("input:radio[name=fear]:checked").value();
-    const visible = $("input:radio[name=visible]:checked").value();
-    const aspire = $("input:radio[name=aspire]:checked").value();
+    const cells = $("input:radio[name=cells]:checked").val();
+    const fun = $("input:radio[name=fun]:checked").val();
+    const fear = $("input:radio[name=fear]:checked").val();
+    const visible = $("input:radio[name=visible]:checked").val();
+    const aspire = $("input:radio[name=aspire]:checked").val();
 
+    const answers = [cells, fun, fear, visible, aspire]
 
+    console.log(answers)
 
   });
 });
